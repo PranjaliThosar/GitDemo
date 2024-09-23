2975c7fa92975a0def64597ae032e52f076c307e
PranjaliThosar <pranjalithosar5@gmail.com>
2024-09-23 16:57:37 +0530

    second commit

--- src/test/java/gpspacksealdemo/Tests/SubmitOrderTestNG.java
+++ src/test/java/gpspacksealdemo/Tests/SubmitOrderTestNG.java
@@ -1,8 +1,29 @@
 package gpspacksealdemo.Tests;
 
-public class SubmitOrderTestNG {
+import java.io.IOException;
+import java.time.Duration;
 
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import gpspacksealdemo.TestComponants.BaseTest;
+import gpspacksealdemo.pageobjects.LandingPage;
+
+public class SubmitOrderTestNG extends BaseTest {
 	
+	public void SubmitOrder() throws IOException, InterruptedException
+	{
+		// TODO Auto-generated method stub
 	
+		WebDriverManager.chromedriver().setup();
+		ChromeDriver driver=new ChromeDriver();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.manage().window().maximize();
+		WebDriverWait wait=new WebDriverWait(driver,Duration.ofSeconds(5));
+		LandingPage landingpage=new LandingPage(driver);
+		landingpage.goTo();
+		landingpage.loginApplication("admin", "123456");
+	
+}
 	
 }
